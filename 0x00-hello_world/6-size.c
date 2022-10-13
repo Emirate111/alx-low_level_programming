c12b0ce78a8e0b549f22f72e1351b190a26fccf9
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-13 12:46:34 -0700

    Test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main -is the entry point for the program.
+ *
+ *Return:zero if no error, Non-zero value  if errors.
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
