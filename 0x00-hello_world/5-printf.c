d8c37d989a1046452eec141d8f0d683d7bec4ba4
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-13 12:44:13 -0700

    Test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
