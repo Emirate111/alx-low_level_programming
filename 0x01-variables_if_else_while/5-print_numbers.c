5dfd8379cff93064f7d1f45b4d0499bd2617dad6
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-15 23:02:40 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 no error, non zero if error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
