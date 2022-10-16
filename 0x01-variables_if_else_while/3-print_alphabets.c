c691d8edd34b7b99f3b5661b2c73863e31283b16
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-15 22:52:17 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 no error, non zero if error.
+ **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
