e4c0ce8d447890ca81c11011cfd364c42caef127
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-19 12:27:44 -0700

    Task 0

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include "MAIN.H"
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return: 0
+ */
+
+int main (void)
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('u');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	('\n');
+	
+	return (0);
+
+}
