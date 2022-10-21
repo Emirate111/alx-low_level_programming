e05d35621e676fc9592474fcce76d945c95927c4
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-21 10:45:49 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	print("Infinite loop avoided! \\0/\n");
+	return (0);
+}
