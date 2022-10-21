ca6dbd590c16c7b7648da95ebaea008070e93cfa
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-21 10:21:26 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_numbers(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
