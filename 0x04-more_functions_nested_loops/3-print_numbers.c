6c59d05a771064f83bf84301dfeac1a673dea36e
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-22 12:52:44 -0700

    T3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print num from 0 - 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
