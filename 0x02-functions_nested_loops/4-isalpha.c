30e2dd3ca68a88eba3e2039c398ef0f7eecd2e60
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-22 03:19:04 -0700

    my task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter, 0  otherwise
  */
-int _isalhpa(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
