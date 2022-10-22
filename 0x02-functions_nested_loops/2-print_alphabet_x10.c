bba169226e591d275211f2060a598985c7b9d25a
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-22 02:41:32 -0700

    my task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'Z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
