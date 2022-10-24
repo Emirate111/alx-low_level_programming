a47d141bed227e5a3b7edb7757898f310cd27a52
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-24 09:18:25 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ *@str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue
+	}
+	_putchar('\n');
+}
