c39be912059feb87932ac8b85e2267438118f5c5
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-24 08:56:18 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input
+ * Return: syring in reverse
+ */
+void rev_string(char *s)
+{
+	char re = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
