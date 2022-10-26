a9474d6138f921bb78a7b2b6bf86eb9a72f3d65c
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-10-26 14:59:06 -0700

    taskc

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   *  only one statement
+   * you are not allowed to code anything else than this line of code
+   */
+  p[5] 
