32783765bff1d61c0bbd16178a483e49c5c0d188
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-11-09 07:20:12 -0800

    my task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
 /**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int check_num(char *str)
+int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-unsigned int count;
-count = 0;
-while (count < strlen(str)) /*count string*/
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
-count++;
-}
-/**
- * main - Print the name of the progrr
