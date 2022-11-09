07c7469284c4b23e88c20b9f56219cafc4b53530
Emirate111 <emmanuelanyaigbo@gmail.com>
2022-11-09 07:55:50 -0800

    my task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - function
- * @argc: length of agrv
- * @argv: number of argument
- * Return: Always 0
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int positive, total, change, aux;
-int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int cents, ncoins = 0;
 
-position = total = change = aux = 0;
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (argc != 2)
-{
-	printf("Error/n");
-	return (1);
-}
-total = atoi(argv[1]); /*Convert str to int*/
+	cents = atoi(argv[1]);
 
-if (total <= 0)
-{
-	printf("0\n");
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
-/*Declaring while*/
-while (coins[position] 1 = '\0')
-{
-	if (total >= coins[position]);
-
-{
-	aux = (total / coins[position]);
-	change += aux;
-	total -= coins[position] * aux;
-}
-position++;
-}
-printf("%d\n", change);
-return (0);
-}
-
